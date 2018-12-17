2c7de7c3820540a6a5547ad23f0a33f1b8bbaa7c
liu.yalei <liu.yalei@puscene.com>
2018-12-17 15:59:22 +0800

    error

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,2 +1,2 @@
 console.log('WePY开启错误监控');
-console.error("CLI报错：\n/Users/mwbyd/Desktop/WepyMall/src/pages/messages.wpy\n  0:0  warning  File ignored because of a matching ignore pattern. Use \"--no-ignore\" to override\n\n✖ 1 problem (0 errors, 1 warning)\n");
+console.error("CLI报错：\n/Users/mwbyd/Desktop/WepyMall/src/pages/authorize.wpy\n  0:0  warning  File ignored because of a matching ignore pattern. Use \"--no-ignore\" to override\n\n✖ 1 problem (0 errors, 1 warning)\n");
